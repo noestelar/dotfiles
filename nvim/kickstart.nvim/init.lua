6e17da02c73dd8e536a7d4b2eeafb59593fbe8c8
Noé Rivera <tsnoe.dali@gmail.com>
2025-08-21 15:06:46 -0600

    add transparency at startup

--- nvim/kickstart.nvim/init.lua
+++ nvim/kickstart.nvim/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -886,8 +886,11 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true, -- Enable transparent background
         styles = {
           comments = { italic = false }, -- Disable italics in comments
+          sidebars = 'transparent', -- Make sidebars transparent
+          floats = 'transparent', -- Make floating windows transparent
         },
       }
 
